565a02859b7f081d29f426a5214a2fe87e2125c3
yji00 <dksro333@naver.com>
2021-06-05 00:29:15 +0900

    slick 시작

---
+++ public/js/index.js
@@ -0,0 +1,19 @@
+	var $main = $('.main-wrapper');
+	var $slick = $main.find('.slide-wrap');
+	$slick.slick();
+
+	// slideMain();
+
+  
+	// function slideMain() {
+	// 		// var $slick = $('.main-wrapper .slide-wrapper');
+	// 		// var $btPrev = $('.main-wrapper .bt-slide.left');
+	// 		// var $btNext = $('.main-wrapper .bt-slide.right');
+	// 		var $main = $('.main-wrapper');
+	// 		var $slick = $main.find('.slide-wrap');
+	// 		var options = cloneObject(slick);
+	// 		options.slidesToShow = 1;
+	// 		options.dots = false;
+	// 		// options.responsive.splice(0,2);
+	// 		$slick.slick(options);
+	// 	}
